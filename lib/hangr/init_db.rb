5c34aac72e6084ecbe2ddd6a641f87ec36d0550d
Brit Butler <redline6561@gmail.com>
2015-10-06 14:08:21 -0400

    Bugfix to the config file path in init_db.

--- lib/hangr/init_db.rb
+++ lib/hangr/init_db.rb
@@ -1,6 +1,6 @@
 require 'active_record'
 require 'yaml'
 
-config_path = '../../config/database.yml'
+config_path = File.expand_path('../../../config/database.yml', __FILE__)
 db_config = YAML::load(File.open(config_path))
 ActiveRecord::Base.establish_connection(db_config)
